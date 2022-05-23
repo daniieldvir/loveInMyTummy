4f14f85695c81877d839489dfb86395bbc382a06
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2022-05-23 17:40:18 +0300

    butten

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,6 @@
 import { Fragment } from 'react';
 import lemonElGryImg from '../../assets/lemonElGryImg.jpg';
+import HeaderCartButton from './HeaderCartButton';
 import classes from './Header.module.css';
 
 const Header = (props) => {
@@ -7,7 +8,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>Love In My Tummy</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
         <img src={lemonElGryImg} alt="Lemon El Gry" />
