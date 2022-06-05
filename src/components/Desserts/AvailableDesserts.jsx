f46c3f1c4066b8ce942556a528f4a6a52e486cf8
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2022-06-05 18:37:58 +0300

    fetchDesserts

--- src/components/Desserts/AvailableDesserts.jsx
+++ src/components/Desserts/AvailableDesserts.jsx
@@ -1,37 +1,37 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import DessertItem from './DessertItem/DessertItem';
 import classes from './AvailableDesserts.module.css';
 
-const DUMMY_DESSERTS = [
-  {
-    id: 'm1',
-    name: 'LPR Pie ',
-    description: 'Lemon Pistachio and Raspberry Pie',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'The pistachio delight',
-    description: 'Pistachio cookies with chocolate and raspberries',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Spring clouds',
-    description: 'Pavlova milkshake with citrus fruits',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Numbers cake',
-    description:
-      'Number-shaped cake with diplomat cream, fruit or chocolates to choose from',
-    price: 18.99,
-  },
-];
-
 const AvailableDesserts = () => {
-  const dessertsList = DUMMY_DESSERTS.map((dessert) => (
+  const [desserts, setDesserts] = useState([]);
+
+  useEffect(() => {
+    const fetchDesserts = async () => {
+      const response = await fetch(
+        'https://desser-a46c4-default-rtdb.firebaseio.com/desserts.json'
+      );
+
+      const responseData = await response.json();
+
+      const loadedDeserts = [];
+
+      for (const key in responseData) {
+        loadedDeserts.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setDesserts(loadedDeserts);
+    };
+
+    fetchDesserts();
+  }, []);
+
+  const dessertsList = desserts.map((dessert) => (
     <DessertItem
       key={dessert.id}
       id={dessert.id}
